9f5176f029d0667a75fc150353e8e1369f7a4563
locnguyent3108 <locnguyent3108@gmail.com>
2019-05-23 13:54:17 +0700

    update deserialize read json file

--- src/main/G1_GettingStarted/main.java
+++ src/main/G1_GettingStarted/main.java
@@ -3,12 +3,14 @@ package G1_GettingStarted;
 import com.google.gson.Gson;
 import com.google.gson.stream.JsonReader;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 
 public class main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         System.out.println("G1- Getting started");
-        serializeUserSimple();
+      //  serializeUserSimple();
         deserializeUserSimple();
     }
 
@@ -20,13 +22,20 @@ public class main {
         String json = gson.toJson(userSimple1);
     }
 
-    private static void deserializeUserSimple(){
+    private static void deserializeUserSimple() throws FileNotFoundException {
         String userA = "{'name':'loc','email':'abc@gmail.com','age':12,'a':12}";
 
         Gson gson = new Gson();
         userSimple userSimpleA = gson.fromJson(userA, userSimple.class);
 
         //Read from json file
-        JsonReader reader = new(JsonReader (new FileReader("userSimple.json")));
+        String jsonPATH = "/Users/macmall/Desktop/javaConner/draft/src/main/G1_GettingStarted/userSimple.json";
+        BufferedReader bufferedReader = new BufferedReader(new  FileReader(jsonPATH));
+
+        userSimple userSimpleB = gson.fromJson(bufferedReader,userSimple.class);
+
+        System.out.println(userSimpleB.getClass());
+
+        System.out.println(userSimpleB.toString());
     }
 }
